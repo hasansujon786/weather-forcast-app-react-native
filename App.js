900cf4e58c8e1d2349f9430749dd08f9c54f3cdf
hasansujon786 <hasansujon786@gmail.com>
2021-07-30 18:25:30 +0600

    feat: add drawer navigation

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import AppLoading from 'expo-app-loading'
 import React from 'react'
 import { StatusBar } from 'react-native'
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
 import Home from './screens/Home'
+import Test from './screens/Test'
 import {
   useFonts,
   Montserrat_400Regular,
@@ -12,6 +17,7 @@ import {
   Montserrat_700Bold_Italic,
 } from "@expo-google-fonts/montserrat"
 
+const Drawer = createDrawerNavigator();
 import { variables } from './theme'
 
 
@@ -34,7 +40,13 @@ export default function App() {
           backgroundColor={variables.colors.accent}
           barStyle="light-content"
         />
-        <Home />
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="Test" component={Test} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+
       </>
     )
   }
